6d8d8e88d812d9cafdd3c1518158da1ecd70fd35
Lxyzy <1728698958@qq.com>
2024-04-25 23:15:11 +0800

    init commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const apiKey = 'd4d84910d86f478fb8d1a0a7ff4e23f2'; // 替换为你的和风天气 API 密钥
+/*function searchCity(){
+    // 模拟一个固定的搜索结果（示例数据）
+    const fixedSearchResult = {
+        cityName: '北京',
+        latitude: '39.9042',
+        longitude: '116.4074'
+    };
+    // 将结果显示在页面上
+    document.getElementById('searchResult').innerText = `城市：${fixedSearchResult.cityName}，经度：${fixedSearchResult.longitude}，纬度：${fixedSearchResult.latitude}`;
+}*/
+//城市搜索 https://geoapi.qweather.com/v2/city/lookup?{查询参数}
+function searchCity(){
+    
+}
